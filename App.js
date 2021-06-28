ebbc6a0480fea703dfa60ebe17ed289a8dedb82a
fabricio <fabricio.alborta.22@gmail.com>
2021-06-28 15:27:25 -0400

    probando component

---
+++ App.js
@@ -0,0 +1,91 @@
+import { Keyboard } from '@material-ui/icons';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  Keyboard,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+
+} from 'react-native';
+
+import { Icon } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  
+  onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert('Credentials', `${username} + ${password}`);
+  }
+
+  render() {
+    return (
+      <TouchableNativeFeedback onPress={() => {
+        Keyboard.dismiss();
+      }}
+      >
+        <LinearGradient
+        colors={['#fff', '#fff', '#fff']}
+        style={styles.container}
+        >
+          <Text style={styles.tituloP}>Registro</Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
+
+        </LinearGradient>
+      </TouchableNativeFeedback>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  tituloP: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fb9e00',
+    alignSelf: 'center',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
